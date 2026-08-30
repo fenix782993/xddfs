825bc3c399741db38ecac89280ebafe988655a8a
fenix782993 <kfeofilov52@gmail.com>
2026-08-30 20:31:32 +0300

    Delete admin.py

--- admin.py
+++
@@ -1,2 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-def profile_menu(): return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='⬅️ Назад',callback_data='home')]])
