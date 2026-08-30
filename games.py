88d569a328adcaeb0497f9342e99849e42a68d96
fenix782993 <kfeofilov52@gmail.com>
2026-08-30 20:20:03 +0300

    Delete games.py

--- games.py
+++
@@ -1,2 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-def pvp_menu(): return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='⚔️ PvP',callback_data='pvp')],[InlineKeyboardButton(text='⬅️ Назад',callback_data='home')]])
