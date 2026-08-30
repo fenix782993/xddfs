662d32b641e3b3e86d18c3c9cff9667c54d07fb6
fenix782993 <kfeofilov52@gmail.com>
2026-08-30 20:33:50 +0300

    Delete web.py

--- web.py
+++
@@ -1,2 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-def admin_menu(): return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='📊 Статистика',callback_data='admin_stats')],[InlineKeyboardButton(text='⬅️ Назад',callback_data='home')]])
